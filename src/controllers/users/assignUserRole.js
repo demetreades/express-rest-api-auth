0121ff18c64d682ba3b96ed2abd2b872ac838252
demetreades <demetreades@protonmail.com>
2021-10-18 23:01:57 +0300

    user roles

---
+++ src/controllers/users/assignUserRole.js
@@ -0,0 +1,3 @@
+"use strict"
+
+// assign role logic 
--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -3,11 +3,15 @@ const getUsers = require('./getUsers');
 const getUserById = require('./getUserById');
 const updateUser = require('./updateUser');
 const deleteUserById = require('./deleteUserById');
+// const revokeUserRole = require('./revokeUserRole');
+// const assignUserRole = require('./assignUserRole');
 
 module.exports = {
   createUser,
   getUsers,
   getUserById,
   updateUser,
-  deleteUserById
+  deleteUserById,
+  // revokeUserRole,
+  // assignUserRole,
 }
