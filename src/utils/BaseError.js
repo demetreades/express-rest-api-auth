dd87971acf6e05cad4e82e66a9682cbd02c811c1
demetreades <demetreades@protonmail.com>
2021-10-19 00:47:31 +0300

    isOperational property added to BaseError

--- src/utils/BaseError.js
+++ src/utils/BaseError.js
@@ -1,7 +1,8 @@
 module.exports = class BaseError extends Error {
-  constructor(message, statusCode) {
+  constructor(message, statusCode, isOperational = false) {
     super(message);
     this.statusCode = statusCode;
+    this.isOperational = isOperational;
 
     Error.captureStackTrace(this, this.constructor);
   }
