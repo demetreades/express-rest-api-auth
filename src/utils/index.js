c0f1a220b2fa5db68aa4f4ff63b46f806e152178
demetreades <demetreades@protonmail.com>
2021-10-19 11:11:44 +0300

    utils index added

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+const BaseError = require('./BaseError');
+const handleExit = require('./handleExit');
+const logger = require('./logger');
+const normalizePort = require('./normalizePort');
+
+module.exports = { BaseError, handleExit, logger, normalizePort };
