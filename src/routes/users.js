f08d041d3584d51406d05f982a472b32f6727b9e
demetreades <demetreades@protonmail.com>
2021-10-19 11:14:16 +0300

    routes path restructure

---
+++ src/routes/users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const router = require('express').Router();
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUserById,
+} = require('../controllers/users');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUserById);
+
+module.exports = router;
