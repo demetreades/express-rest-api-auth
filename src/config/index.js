799b07ef981f9ca74f37315dd8598bc8358ef5cb
demetreades <demetreades@protonmail.com>
2021-10-20 21:03:57 +0300

    passport , sessionOptions added

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,12 @@
 const corsOptions = require('./cors/corsOptions');
 const limitOptions = require('./limitOptions');
+const sessionOptions = require('./sessionOptions');
 const dbConnection = require('./db/connection');
+const passport = require('./passport');
 
 module.exports = {
   corsOptions,
   limitOptions,
   dbConnection,
+  passport,
 };
