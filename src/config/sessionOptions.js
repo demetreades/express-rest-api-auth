27bbf1076442cad48612635369d85de40ab7dfcb
demetreades <demetreades@protonmail.com>
2021-10-20 21:03:23 +0300

    MongoDBStore , session configuration

---
+++ src/config/sessionOptions.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('dotenv').config();
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const dbConnection = require('./db/connection');
+
+const sessionStore = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  mongooseConnection: dbConnection,
+  collection: 'sessions',
+});
+
+module.exports = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    secure: false,
+  },
+};
