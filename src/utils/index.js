0c38e3ef76aded3c0971b5759e938254019087da
demetreades <demetreades@protonmail.com>
2021-10-22 00:24:32 +0300

    moved from middleware

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,17 @@
-const BaseError = require('./BaseError');
-const handleExit = require('./handleExit');
 const logger = require('./logger');
+const BaseError = require('./errors/BaseError');
+const handleNotFound = require('./errors/handleNotFound');
+const handleMongoErrors = require('./errors/handleMongoErrors');
+const handleErrors = require('./errors/handleErrors');
+const handleExit = require('./handleExit');
 const normalizePort = require('./normalizePort');
 
-module.exports = { BaseError, handleExit, logger, normalizePort };
+module.exports = {
+  logger,
+  BaseError,
+  handleNotFound,
+  handleMongoErrors,
+  handleErrors,
+  handleExit,
+  normalizePort,
+};
