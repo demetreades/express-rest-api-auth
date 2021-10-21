0201b715e0fd7ad3eb9e740ca679d535550156bf
demetreades <demetreades@protonmail.com>
2021-10-22 00:36:39 +0300

    import paths refactor

--- src/app.js
+++ src/app.js
@@ -2,24 +2,15 @@
 
 require('dotenv').config();
 const express = require('express');
-// const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const session = require('express-session');
-const passport = require('passport');
+// const session = require('express-session');
+// const passport = require('passport');
+const dbConnection = require('./services/database/connection');
 
-const {
-  handleErrors,
-  handleMongoErrors,
-  handleNotFound,
-} = require('./middleware');
-const {
-  dbConnection,
-  corsOptions,
-  limitOptions,
-  sessionOptions,
-} = require('./config');
+const { handleErrors, handleMongoErrors, handleNotFound } = require('./utils');
+const { corsOptions, limitOptions, sessionOptions } = require('./config');
 
 const userRoutes = require('./routes/users');
 
@@ -31,12 +22,11 @@ app.use(helmet());
 app.use(rateLimit(limitOptions));
 app.use(cors(corsOptions));
 
-// app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.get('/', (req, res) => res.json({ message: 'development root endpoint' }));
 app.use('/api/users', userRoutes);
