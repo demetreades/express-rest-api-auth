c2b920ba4051983d149ef119292adff63798ba8f
demetreades <demetreades@protonmail.com>
2021-10-22 00:39:03 +0300

    dbConnection moved to services

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,11 @@
 const corsOptions = require('./cors/corsOptions');
 const limitOptions = require('./limitOptions');
 const sessionOptions = require('./sessionOptions');
-const dbConnection = require('./db/connection');
 const passport = require('./passport');
 
 module.exports = {
   corsOptions,
   limitOptions,
-  dbConnection,
   passport,
-  sessionOptions
+  sessionOptions,
 };
