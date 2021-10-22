4e55e023cd9345711395eca6f2fce75276113fa1
demetreades <demetreades@protonmail.com>
2021-10-22 19:17:37 +0300

    basic authorization with passport added

--- src/app.js
+++ src/app.js
@@ -5,18 +5,23 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-// const session = require('express-session');
-// const passport = require('passport');
-const dbConnection = require('./services/database/connection');
+const session = require('express-session');
+const passport = require('passport');
 
+require('./config/passport/localStrategy');
+
+const dbConnection = require('./services/database/connection');
 const { handleErrors, handleMongoErrors, handleNotFound } = require('./utils');
 const { corsOptions, limitOptions, sessionOptions } = require('./config');
 
 const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
 
 dbConnection();
 
 const app = express();
+
+app.use(session(sessionOptions));
 
 app.use(helmet());
 app.use(rateLimit(limitOptions));
@@ -25,11 +30,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => res.json({ message: 'development root endpoint' }));
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => res.json({ message: 'development root endpoint' }));
 
 app.use(handleNotFound);
 app.use(handleMongoErrors);
