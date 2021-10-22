aa7aac3791600f45aa6ed7ea60babf9c203b8efe
demetreades <demetreades@protonmail.com>
2021-10-22 19:20:54 +0300

    passport removed

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,9 @@
 const { corsOptions } = require('./cors/corsOptions');
 const { limitOptions } = require('./limitOptions');
 const { sessionOptions } = require('./passport/sessionOptions');
-const passport = require('./passport/passport');
 
 module.exports = {
   corsOptions,
   limitOptions,
-  passport,
   sessionOptions,
 };
