7acd9d8ab7efaeffa60f463c19add5b6690dddc2
demetreades <demetreades@protonmail.com>
2021-10-22 19:23:11 +0300

    fallback code 500 added for statusCode logging message

--- src/utils/errors/handleErrors.js
+++ src/utils/errors/handleErrors.js
@@ -7,7 +7,11 @@ module.exports = (err, req, res, next) => {
   const { statusCode, message } = err;
   const { method } = req;
 
-  logger.error(`ERROR: ${method} ${statusCode} :: ${message}`);
+  logger.error(
+    `ERROR: ${method} ${
+      statusCode || StatusCodes.INTERNAL_SERVER_ERROR
+    } :: ${message}`
+  );
   // logger.error(`${err.stack}`); // stack
 
   res.status(statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
