3bb62548410c50dcd7495579239f2b99e43c3add
demetreades <demetreades@protonmail.com>
2021-10-23 11:54:52 +0300

    logout route added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,9 +4,11 @@ const { Router } = require('express');
 const passport = require('passport');
 
 const loginUser = require('../controllers/auth/loginUser');
+const logOutUser = require('../controllers/auth/logOutUser');
 
 const router = Router();
 
 router.post('/login', passport.authenticate('local'), loginUser);
+router.get('/logout', logOutUser);
 
 module.exports = router;
