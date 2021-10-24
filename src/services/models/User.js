cb5d08e0a0cf8f0349db82c7e3dd76cfb312342e
demetreades <demetreades@protonmail.com>
2021-10-24 13:20:59 +0300

    User model cleasing

--- src/services/models/User.js
+++ src/services/models/User.js
@@ -2,7 +2,6 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-// const emailValidator = require('express-email-validation'); // mporei na einai kai lathos to path
 
 const userSchema = mongoose.Schema(
   {
@@ -10,6 +9,7 @@ const userSchema = mongoose.Schema(
       type: String,
       trim: true,
       unique: true,
+      lowercase: true,
       required: [true, 'Username is required'],
       minlength: [3, 'Username cannot be less than 3 characters'],
       maxlength: [64, 'Username cannot exceed 64 characters'],
@@ -18,20 +18,16 @@ const userSchema = mongoose.Schema(
       type: String,
       trim: true,
       unique: true,
-      required: [true, 'Email is required'],
       lowercase: true,
-      maxlength: [100, 'Email cannot exceed 120 characters'],
-      // validate: {
-      //   validator: emailValidator.validate,
-      //   message: (input) => `${input.value} is not a valid email address!`,
-      // },
+      required: [true, 'Email is required'],
+      maxlength: [120, 'Email cannot exceed 120 characters'],
     },
     password: {
       type: String,
       trim: true,
       required: [true, 'Password is required'],
       maxlength: [25, 'Password cannot exceed 25 characters'],
-      minlength: [6, 'Password cannot be less than 6 characters'],
+      minlength: [8, 'Password cannot be less than 6 characters'],
       // select: false,
     },
 
@@ -45,9 +41,6 @@ const userSchema = mongoose.Schema(
     //   enum: ['User', 'Editor', 'Admin'],
     //   default: 'User',
     // },
-
-    // resetPasswordToken: String,
-    // resetPasswordExpire: Date,
   },
 
   {
@@ -69,32 +62,3 @@ userSchema.pre('save', async function (next) {
 });
 
 module.exports = mongoose.model('User', userSchema);
-
-// // Sign JWT and return
-// userSchema.methods.getSignedJwtToken = function () {
-//   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRE,
-//   });
-// };
-
-// // Match user entered password to hashed password in database
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// // Generate and hash password token
-// userSchema.methods.getResetPasswordToken = function () {
-//   // Generate token
-//   const resetToken = crypto.randomBytes(20).toString('hex');
-
-//   // Hash token and set to resetPasswordToken field
-//   this.resetPasswordToken = crypto
-//     .createHash('sha256')
-//     .update(resetToken)
-//     .digest('hex');
-
-//   // Set expire
-//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
-
-//   return resetToken;
-// };
