effa69b57e7bffb41a83b90675c498aa80746323
demetreades <demetreades@protonmail.com>
2021-10-24 13:18:14 +0300

    getProfile route added in auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,12 +3,14 @@
 const { Router } = require('express');
 const passport = require('passport');
 
-const loginUser = require('../controllers/auth/loginUser');
-const logOutUser = require('../controllers/auth/logOutUser');
+const {
+  authController: { loginUser, logOutUser, getProfile },
+} = require('../controllers');
 
 const router = Router();
 
 router.post('/login', passport.authenticate('local'), loginUser);
 router.get('/logout', logOutUser);
+router.get('/profile', getProfile);
 
 module.exports = router;
