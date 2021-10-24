e8c3fa1e42fca5f33a0ea948789ea73e6fa56952
demetreades <demetreades@protonmail.com>
2021-10-24 15:24:35 +0300

    loginUser refactored

--- src/controllers/auth/loginUser.js
+++ src/controllers/auth/loginUser.js
@@ -1,13 +1,34 @@
 'use strict';
 
 const { StatusCodes } = require('http-status-codes');
-const { logger } = require('../../utils');
+const { validateUsername } = require('../../services/joiSchema');
+const userService = require('../../services/user');
+const { logger, BaseError } = require('../../utils');
 
 module.exports = async (req, res, next) => {
   try {
     const { username } = req.body;
 
+    const { username: result } = await validateUsername({ username });
+
+    const user = await userService.getByProperty(result);
+
     logger.info(`CONTROLLER: ${username}, logged in`);
+
+    req.login(user, (err) => {
+      if (err) {
+        if (err.isJoi === true) {
+          return next(
+            new BaseError(
+              err.details[0].message,
+              StatusCodes.UNPROCESSABLE_ENTITY
+            )
+          );
+        }
+
+        return next(err);
+      }
+    });
 
     res.status(StatusCodes.ACCEPTED).json({
       success: true,
