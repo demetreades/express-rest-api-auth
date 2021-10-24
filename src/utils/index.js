d69bb77035ad219afb5d747bd2604a206c1989b3
demetreades <demetreades@protonmail.com>
2021-10-24 13:19:46 +0300

    refactored

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,19 @@
-const logger = require('./logger');
 const BaseError = require('./errors/BaseError');
 const handleNotFound = require('./errors/handleNotFound');
 const handleMongoErrors = require('./errors/handleMongoErrors');
 const handleErrors = require('./errors/handleErrors');
+
+const logger = require('./logger');
 const handleExit = require('./handleExit');
 const normalizePort = require('./normalizePort');
 
 module.exports = {
-  logger,
   BaseError,
   handleNotFound,
   handleMongoErrors,
   handleErrors,
+  
+  logger,
   handleExit,
   normalizePort,
 };
