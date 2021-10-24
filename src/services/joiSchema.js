9c898814332316fdffd39b4bf5ef37899dc9ab62
demetreades <demetreades@protonmail.com>
2021-10-24 13:18:24 +0300

    joi schemas added

---
+++ src/services/joiSchema.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Joi = require('@hapi/joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const validateId = (id) => {
+  const idValidationSchema = Joi.object({
+    _id: Joi.objectId().required(),
+  });
+
+  return idValidationSchema.validateAsync(id);
+};
+
+const validateUser = (user) => {
+  const userValidationSchema = Joi.object({
+    username: Joi.string().lowercase().min(3).max(64).required(),
+    email: Joi.string().email().lowercase().max(120).required(),
+    password: Joi.string().min(8).max(25).required(),
+  });
+
+  return userValidationSchema.validateAsync(user);
+};
+
+module.exports = {
+  validateUser,
+  validateId,
+};
